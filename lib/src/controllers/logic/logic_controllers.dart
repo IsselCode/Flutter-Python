2fdca3601b2e63a236d77083f319efa41a956d6c
issel <isselcode@gmail.com>
2025-10-07 19:44:02 -0600

    Archivo de exportación de controladores para importaciones más limpias

---
+++ lib/src/controllers/logic/logic_controllers.dart
@@ -0,0 +1,3 @@
+export 'package:flutter_python_prueba/src/controllers/logic/bounding_controller.dart';
+export 'package:flutter_python_prueba/src/controllers/logic/camera_controller.dart';
+export 'package:flutter_python_prueba/src/controllers/logic/device_controller.dart';
--- lib/src/views/home_view.dart
+++ lib/src/views/home_view.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_python_prueba/core/utils/random_hex_color.dart';
 import 'package:flutter_python_prueba/src/controllers/logic/bounding_controller.dart';
 import 'package:flutter_python_prueba/src/controllers/logic/camera_controller.dart';
 import 'package:flutter_python_prueba/src/widgets/bbox_editor/bbox_editor.dart';
 import 'package:flutter_python_prueba/src/widgets/bbox_editor/bbox_editor_controller.dart';
 import 'package:flutter_python_prueba/src/widgets/bbox_editor/bbox_editor_events.dart';
-import 'package:flutter_python_prueba/src/widgets/bbox_editor/bbox_entity.dart';
 import 'package:provider/provider.dart';
 
 import '../widgets/bbox_editor/bbox_editor_enums.dart';
